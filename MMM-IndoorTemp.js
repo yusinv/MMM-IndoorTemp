0e6d8b3dc8435e55d0e6c7a09f6119db68340d7f
Valentin <yusinv@gmail.com>
2018-09-30 19:19:27 +0300

    added humidity

--- MMM-IndoorTemp.js
+++ MMM-IndoorTemp.js
@@ -12,66 +12,80 @@
 
 Module.register('MMM-IndoorTemp', {
 
-  defaults: {
-    mqttServer: 'mqtt://test.mosquitto.org',
-    topic: '',
-    interval: 150000,
-    debug: false,
-    showAlerts: true,
-    showTitle: false,
-    title: 'Indoor Temperature',
-    loadingText: 'Loading MQTT Data...',
-    postText: ''
-  },
+    defaults: {
+        mqttServer: 'mqtt://test.mosquitto.org',
+        tempTopic: '',
+        humidityTopic: '',
+        interval: 150000,
+        debug: false,
+        showAlerts: true,
+        showTitle: false,
+        title: 'Indoor Temperature/Humidity',
+        loadingText: 'Loading MQTT Data...',
+        postText: ''
+    },
 
-  start: function() {
-    Log.info('Starting module: ' + this.name);
-    this.loaded = false;
-    this.mqttVal = '';
-    this.updateMqtt(this);
-  },
+    start: function () {
+        Log.info('Starting module: ' + this.name);
+        this.loaded = false;
+        this.mqttVal = '';
+        this.updateMqtt(this);
+    },
 
-  updateMqtt: function(self) {
-    self.sendSocketNotification('MQTT_SERVER', { mqttServer: self.config.mqttServer, topic: self.config.topic });
-    setTimeout(self.updateMqtt, self.config.interval, self);
-  },
+    updateMqtt: function (self) {
+        self.sendSocketNotification('MQTT_SERVER', {
+            mqttServer: self.config.mqttServer,
+            tempTopic: self.config.tempTopic,
+            humidityTopic: self.config.humidityTopic
+        });
+        setTimeout(self.updateMqtt, self.config.interval, self);
+    },
 
-  getDom: function() {
-    var wrapper = document.createElement('div');
+    getDom: function () {
+        var wrapper = document.createElement('div');
 
-    if (this.config.debug) {
+        if (this.config.debug) {
 
-      if (!this.loaded) {
-        wrapper.innerHTML = this.config.loadingText;
+            if (!this.loaded) {
+                wrapper.innerHTML = this.config.loadingText;
+                return wrapper;
+            }
+
+            var titleDiv = document.createElement('div');
+            titleDiv.innerHTML = this.config.title;
+            wrapper.appendChild(titleDiv);
+
+            var mqttDiv = document.createElement('div');
+            mqttDiv.innerHTML = this.mqttVal.toString().concat(this.config.postText);
+            wrapper.appendChild(mqttDiv);
+
+        }
+
         return wrapper;
-      }
+    },
 
-      var titleDiv = document.createElement('div');
-      titleDiv.innerHTML = this.config.title;
-      wrapper.appendChild(titleDiv);
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === 'MQTT_DATA' && payload.topic === this.config.tempTopic) {
+            this.mqttVal = payload.data.toString();
+            this.loaded = true;
+            this.sendNotification('INDOOR_TEMPERATURE', this.mqttVal);
+            if (this.config.debug) {
+                this.updateDom();
+            }
+        }
 
-      var mqttDiv = document.createElement('div');
-      mqttDiv.innerHTML = this.mqttVal.toString().concat(this.config.postText);
-      wrapper.appendChild(mqttDiv);
+        if (notification === 'MQTT_DATA' && payload.topic === this.config.humidityTopic) {
+            this.mqttVal = payload.data.toString();
+            this.loaded = true;
+            this.sendNotification('INDOOR_HUMIDITY', this.mqttVal);
+            if (this.config.debug) {
+                this.updateDom();
+            }
+        }
 
+        if (notification === 'ERROR' && this.config.showAlerts) {
+            this.sendNotification('SHOW_ALERT', payload);
+        }
     }
-
-    return wrapper;
-  },
-
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === 'MQTT_DATA' && payload.topic === this.config.topic) {
-      this.mqttVal = payload.data.toString();
-      this.loaded = true;
-      this.sendNotification('INDOOR_TEMPERATURE', this.mqttVal);
-      if (this.config.debug) {
-        this.updateDom();
-      }
-    }
-
-    if (notification === 'ERROR' && this.config.showAlerts){
-      this.sendNotification('SHOW_ALERT', payload);
-    }
-  }
 
 });
--- node_helper.js
+++ node_helper.js
@@ -13,59 +13,59 @@ const NodeHelper = require('node_helper');
 var mqtt = require('mqtt');
 
 module.exports = NodeHelper.create({
-  start: function() {
-    console.log('MMM-IndoorTemp started ...');
-    this.clients = [];
-  },
+    start: function () {
+        console.log('MMM-IndoorTemp started ...');
+        this.clients = [];
+    },
 
-  connectMqtt: function(config) {
-    var self = this;
-    var client;
+    connectMqtt: function (config) {
+        var self = this;
+        var client;
 
-    if(typeof self.clients[config.mqttServer] === "undefined") {
-      console.log("Creating new MQTT client for url: ", config.mqttServer);
-      client = mqtt.connect(config.mqttServer);
-      self.clients[config.mqttServer] = client;
+        if (typeof self.clients[config.mqttServer] === "undefined") {
+            console.log("Creating new MQTT client for url: ", config.mqttServer);
+            client = mqtt.connect(config.mqttServer);
+            self.clients[config.mqttServer] = client;
 
-      client.on('error', function(error) {
-        console.log('*** MQTT JS ERROR ***: ' + error);
-        self.sendSocketNotification('ERROR', {
-          type: 'notification',
-          title: 'MQTT Error',
-          message: 'The MQTT Client has suffered an error: ' + error
-        });
-      });
+            client.on('error', function (error) {
+                console.log('*** MQTT JS ERROR ***: ' + error);
+                self.sendSocketNotification('ERROR', {
+                    type: 'notification',
+                    title: 'MQTT Error',
+                    message: 'The MQTT Client has suffered an error: ' + error
+                });
+            });
 
-      client.on('offline', function() {
-        console.log('*** MQTT Client Offline ***');
-        self.sendSocketNotification('ERROR', {
-          type: 'notification',
-          title: 'MQTT Offline',
-          message: 'MQTT Server is offline.'
-        });
-        client.end();
-      });
-    } else {
-      client = self.clients[config.mqttServer];
+            client.on('offline', function () {
+                console.log('*** MQTT Client Offline ***');
+                self.sendSocketNotification('ERROR', {
+                    type: 'notification',
+                    title: 'MQTT Offline',
+                    message: 'MQTT Server is offline.'
+                });
+                client.end();
+            });
+        } else {
+            client = self.clients[config.mqttServer];
+        }
+
+        if (config.mode !== 'send') {
+            client.subscribe(config.tempTopic);
+            client.subscribe(config.humidityTopic);
+            client.on('message', function (topic, message) {
+                self.sendSocketNotification('MQTT_DATA', {'topic': topic, 'data': message.toString()});
+            });
+        }
+    },
+
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === 'MQTT_SERVER') {
+            this.connectMqtt(payload);
+        } else if (notification == 'MQTT_SEND') {
+            var client = this.clients[payload.mqttServer];
+            if (typeof client !== "undefined") {
+                client.publish(payload.topic, JSON.stringify(payload.payload));
+            }
+        }
     }
-
-    if(config.mode !== 'send') {
-      client.subscribe(config.topic);
-
-      client.on('message', function(topic, message) {
-        self.sendSocketNotification('MQTT_DATA', {'topic':topic, 'data':message.toString()});
-      });
-    }
-  },
-
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === 'MQTT_SERVER') {
-      this.connectMqtt(payload);
-    } else if(notification == 'MQTT_SEND') {
-      var client = this.clients[payload.mqttServer];
-      if(typeof client !== "undefined") {
-        client.publish(payload.topic, JSON.stringify(payload.payload));
-      }
-    }
-  }
 });
